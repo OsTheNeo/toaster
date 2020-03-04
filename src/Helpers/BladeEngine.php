bbb0425678d9f5ac59d06256833e4e74d76a9619
baironbernal <baironbernal263@gmail.com>
2020-03-04 17:17:11 -0500

    ckeditor

--- src/Helpers/BladeEngine.php
+++ src/Helpers/BladeEngine.php
@@ -265,7 +265,7 @@ class BladeEngine {
                 return $construct;
                 break;
             case 'ckeditor':
-                $construct->field = Form::textarea('ckeditor', null, array_merge(['rows' => '3', 'class' => 'uk-textarea'],$options));
+                $construct->field = Form::textarea('ckeditor', null, array_merge(['rows' => '3', 'class' => 'ckeditor', 'id' => 'ckeditor'],$options));
                 return $construct;
                 break;
         }
